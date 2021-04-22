1949456140bbf1e37bef8e838c59b4342d577b1e
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2021-04-22 13:47:58 +0800

    ✨ feat: add if-else statements to display random .img2

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ if (randomNumber1 === 1) {
 } else {
     document.querySelector(".img1").setAttribute("src", "images/dice6.png");
 }
+
+if (randomNumber2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+} else if (randomNumber2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+} else if (randomNumber2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+} else if (randomNumber2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+} else if (randomNumber2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+} else {
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
