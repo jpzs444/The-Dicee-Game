a4258577c7f0c60f3d0497f60c2c678e218c12b7
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2021-04-22 13:46:48 +0800

    ✨ feat: create randomNumber2 variable

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 if (randomNumber1 === 1) {
     document.querySelector(".img1").setAttribute("src", "images/dice1.png");
